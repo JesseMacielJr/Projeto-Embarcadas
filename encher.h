273d6228762ecc0b33464acc357c9315f86205c9
JesseJunior <47272727+JesseMacielJr@users.noreply.github.com>
2020-06-23 13:04:14 -0300

    Add files via upload

---
+++ encher.h
@@ -0,0 +1,7 @@
+#ifndef ENCHER_H
+#define	ENCHER_H
+
+void encher_tanque(double *);
+
+#endif	/* ENCHER_H */
+
---
+++ ver.h
@@ -0,0 +1,7 @@
+#ifndef VER_H
+#define	VER_H
+
+void ver_quantidade(double *);
+
+#endif	/* VER_H */
+
